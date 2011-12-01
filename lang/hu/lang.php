2dc66a94ed9c19dceea1297842a07e3b9c8d3cd1
Hendry Badao <hendrybadao@gmail.com>
2011-12-01 20:05:18 +0300

    NEW: added the hungarian language files

---
+++ lang/hu/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Hungarian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Jozsef Lukacs <jozsef.lukacs@gmail.com>
+ */
+
+// custom language strings for the plugin
+
+$lang['yearbox_months'] = array('Jan', 'Feb', 'Már', 'Ápr', 'Máj', 'Jún',
+                                'Júl', 'Aug', 'Sze', 'Okt', 'Nov', 'Dec');
+// week begins on Sunday in PHP...
+$lang['yearbox_days'] = "VHKSCPSV";
+
