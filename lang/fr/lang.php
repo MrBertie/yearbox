fc76c9edb9922580238b6d5cc23b267df2ef95ab
Hendry Badao <hendrybadao@gmail.com>
2012-03-03 14:21:40 +0300

    added new language: french

---
+++ lang/fr/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * French language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Symon Bent <symonbent@gmail.com>
+ */
+
+// custom language strings for the plugin
+
+$lang['yearbox_months'] = array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+                                'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+// week begins on Sunday in PHP... càd Dimanche
+$lang['yearbox_days'] = "DLMMJVS";
+?>
