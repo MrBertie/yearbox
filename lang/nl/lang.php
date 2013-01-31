63df82e5a72fa5fb0b551d789e40370458a2ee06
Max Westen <max@dlmax.org>
2013-01-31 11:20:06 +0100

    Added dutch language-file

---
+++ lang/nl/lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Dutch language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Max Westen <max@dlmax.org>
+ */
+
+// custom language strings for the plugin
+
+$lang['yearbox_months'] = array('Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun',
+  'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec');
+
+// week begins on Sunday in PHP...
+$lang['yearbox_days'] = "ZMDWDVZ";
